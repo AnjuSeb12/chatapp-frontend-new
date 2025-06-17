319b91882cb02ed684684ba1c0de6f804e2cadd4
Anju Sebastian <christeenaanju@gmail.com>
2025-06-17 16:21:03 +0530

    updated

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -88,9 +88,9 @@ export const useAuthStore = create((set, get) => ({
     if (!authUser || get().socket?.connected) return;
 
     const socket = io(BASE_URL, {
-      query: {
-        userId: authUser._id,
-      },
+      // query: {
+      //   userId: authUser._id,
+      // },
         withCredentials: true,
     });
     socket.connect();
