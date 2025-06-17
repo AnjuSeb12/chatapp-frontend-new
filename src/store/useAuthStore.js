855b281772cbad230aa5acac449b2e8ce3c53475
Anju Sebastian <christeenaanju@gmail.com>
2025-06-17 14:49:19 +0530

    inital git repo

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -3,7 +3,8 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+// const BASE_URL = import.meta.env.MODE === "development" ? import.meta.env.VITE_API_BASE_URL : "/";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
@@ -90,6 +91,7 @@ export const useAuthStore = create((set, get) => ({
       query: {
         userId: authUser._id,
       },
+        withCredentials: true,
     });
     socket.connect();
 
