5fc49e3c9c4df0f567f2db42eaecd3a89dcc4932
Anju Sebastian <christeenaanju@gmail.com>
2025-06-21 16:59:42 +0530

    updated

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -4,7 +4,8 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
 // const BASE_URL = import.meta.env.MODE === "development" ? import.meta.env.VITE_API_BASE_URL : "/";
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+// const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
@@ -109,7 +110,7 @@ export const useAuthStore = create((set, get) => ({
   const { authUser } = get();
   if (!authUser || get().socket?.connected) return;
 
-  const socket = io(BASE_URL, {
+  const socket = io(SOCKET_URL, {
     withCredentials: true,           // ✅ Ensure cookie is sent
     transports: ["websocket"],       // ✅ Force websocket transport (Render supports it)
   });
